fa0a6d6e76c78ba44aa6790ca2bcf83ca71cf39c
dolonakter61 <44018201+dolonakter61@users.noreply.github.com>
2018-10-19 01:54:19 +0600

    Add files via upload

---
+++ D_1002.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	float r;
+
+	scanf("%f", &r);
+	printf("A=%.4f\n", r*r*3.14159);
+
+	return 0;
+
+}
---
+++ D_1004.c
@@ -0,0 +1,14 @@
+//Pro
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("PROD = %d\n", a * b);
+
+	return 0;
+}
